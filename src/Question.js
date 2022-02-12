4c33275f978c074cec57e8f042f3b00e96d9e088
Devanshu <devanshudesai_1509@yahoo.com>
2022-02-12 17:17:21 +0530

    first commit

---
+++ src/Question.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+function Question() {
+  return (
+    <div>Question</div>
+  )
+}
+
+export default Question
