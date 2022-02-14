a69be804131ba23d2483d41e2c85410f4a066066
Devanshu <devanshudesai_1509@yahoo.com>
2022-02-14 12:07:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import data from './data';
+import SingleQuestion from "./Question";
 
 function App() {
+
+  const [questions, setQuestions] = useState(data)
+
   return (
-    <div className="App">
-      Q and A page
-    </div>
+    <main>
+      <div className="container">
+        <h3>Some of the frequently asked Questions...</h3>
+      <section className="info">
+          {
+            questions.map((quest) => {
+              return <SingleQuestion key={quest.id} {...quest} />
+            })
+          }
+      </section>
+      </div>
+    </main>
   );
 }
 
